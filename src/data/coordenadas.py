9ffea2a64f7532e6b77704cadd4f30a817bcdafe
Joao Pedro Goncalves Carazzato <joao.carazzato@sou.inteli.edu.br>
2023-03-28 23:02:19 -0300

    SQLAlchemy Adition to Flask

--- src/data/coordenadas.py
+++
@@ -1,6 +0,0 @@
-class Coordenada():
-    def __init__(self, x, y, z, r) -> None:
-        self.x = x
-        self.y = y
-        self.z = z
-        self.r = r
--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request, redirect
-from data.coordenadas import Coordenada
-
-coordenadas =[
-    Coordenada(x="0", y="0", z="0", r="0"),
-]
-
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coordenada.db'
+db = SQLAlchemy(app)
+@app.cli.command()
+def createdb():
+    db.create_all()
+
+class Coordenada(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    x = db.Column(db.Float)
+    y = db.Column(db.Float)
+    z = db.Column(db.Float)
+    r = db.Column(db.Float)
 
 @app.route("/")
 def index():
+    global coordenadas
+    coordenadas = Coordenada.query.all()
     return render_template("index.html", coordenadas=coordenadas)
 
 @app.route('/coords', methods=["POST"])
@@ -19,19 +28,19 @@ def enviar_coords():
         z= request.form["z"],
         r= request.form["r"]
     )
-    global coordenadas
-    coordenadas.append(coords)
+    db.session.add(coords)
+    db.session.commit()
     return redirect("/")
 
 @app.route('/godot', methods=["GET", "POST"])
 def godot_coords():
-    x = coordenadas[(len(coordenadas) - 1)].x
-    y = coordenadas[(len(coordenadas) - 1)].y
-    z = coordenadas[(len(coordenadas) - 1)].z
-    r = coordenadas[(len(coordenadas) - 1)].r
+    coordenadas = Coordenada.query.all()
+    x = coordenadas[-1].x
+    y = coordenadas[-1].y
+    z = coordenadas[-1].z
+    r = coordenadas[-1].r
     godotstring = f"{x}/{y}/{z}/{r}"
     print(godotstring)
     return godotstring
-
 
 app.run(host ="0.0.0.0", port=3000, debug=True)
